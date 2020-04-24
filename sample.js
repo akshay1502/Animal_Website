84d86e25183f19a0caeb30b6e04e5f5e277bd495
akshay1502 <akshayshinde7289@gmail.com>
2020-04-24 18:31:34 +0530

    addding new files

---
+++ sample.js
@@ -0,0 +1,34 @@
+﻿var a=[
+	{
+		username:"akshay",
+		password:"24"
+	}
+];
+
+var news=[
+	{
+	 user:"anjali",
+	 post:"feeling happy"
+	},
+	{
+	 user:"rajesh",
+	 post:"i m rich"
+	}
+]
+
+var usernameprompt=prompt("enter username");
+var passwordprompt=prompt("enter password");
+
+signIn(usernameprompt,passwordprompt);
+
+function signIn(user, pass){
+	if(user === a[0].username && pass === a[0].password)
+	{
+		console.log(news);
+	}
+	else
+	{
+	 alert("incorrect username or password");
+	}
+
+}
